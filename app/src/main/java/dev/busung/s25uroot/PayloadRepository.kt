086cf8b124ee2b07c8d67e3fe1612d00addfc529
zainarbani <zaintsyariev@gmail.com>
2026-08-20 01:04:55 +0700

    Point to own repos
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
+++ app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
@@ -137,10 +137,10 @@ class PayloadRepository(private val context: Context) {
 
     companion object {
         private const val COMMIT_API_URL =
-            "https://api.github.com/repos/BuSung-dev/Root-My-Galaxy-Payloads/git/ref/heads/main"
+            "https://api.github.com/repos/zainarbani/Root-My-Galaxy-Payloads/git/ref/heads/a54x-test"
         private const val RAW_REPOSITORY =
-            "https://raw.githubusercontent.com/BuSung-dev/Root-My-Galaxy-Payloads"
-        private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/main/"
+            "https://raw.githubusercontent.com/zainarbani/Root-My-Galaxy-Payloads"
+        private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/a54x-test/"
         private const val MAX_COMMIT_RESPONSE_BYTES = 16 * 1024
         private const val MAX_MANIFEST_BYTES = 256 * 1024
     }
